4b10d9d7142d7f51ed85eb90377b359999660031
Muhammad Talha <64118988+talhawaheed152@users.noreply.github.com>
2023-11-28 17:16:33 +0100

    Update exercise2.py

--- exercises/exercise2.py
+++ exercises/exercise2.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from sqlalchemy import create_engine, Column, Table, MetaData, Integer, BigInteger, Text, Float, String
+from sqlalchemy import create_engine, Column, Table, MetaData, Integer, BIGINT, TEXT, FLOAT, String
 
 df=pd.read_csv('https://download-data.deutschebahn.com/static/datasets/haltestellen/D_Bahnhof_2020_alle.CSV',delimiter=';')
 # Dropping the Status
@@ -20,15 +20,15 @@ engine = create_engine('sqlite:///trainstops.sqlite', echo=True)
 meta = MetaData()
 trainstops = Table(
     'trainstops', meta,
-    Column('EVA_NR', Integer),
-    Column('TDSA', String),
-    Column('IFOPT', String),
-    Column('NAME', String),
-    Column('Verkehr', String),
-    Column('Laenge', Float),
-    Column('Breite', Float),
-    Column('Betreiber_Name', String),
-    Column('Betreiber_Nr', Integer)
+    Column('EVA_NR', BIGINT),
+    Column('TDSA', TEXT),
+    Column('IFOPT', TEXT),
+    Column('NAME', TEXT),
+    Column('Verkehr', TEXT),
+    Column('Laenge', FLOAT),
+    Column('Breite', FLOAT),
+    Column('Betreiber_Name', TEXT),
+    Column('Betreiber_Nr', FLOAT)
 )
 meta.create_all(engine)
 conn = engine.connect()
